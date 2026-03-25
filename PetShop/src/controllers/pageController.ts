e6239b1616fbe4cf34442f7d3e520bc33fde202f
Artêmis M. Lima <artemismlima@gmail.com>
2026-03-25 08:08:08 -0300

    inicializado

---
+++ PetShop/src/controllers/pageController.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
+
+export const home = (req: Request, res: Response) => {
+	res.render('pages/page', {
+		menu: createMenuObject('all'),
+		banner: {
+			title: 'Todos os animais',
+			background: 'allanimals.jpg'
+		}
+	});
+};
+
+export const dogs = (req: Request, res: Response) => {
+	let list = Pet.getFromType('dog')
+	res.render('pages/page', {
+		menu: createMenuObject('dogs'),
+		banner: {
+			title: 'Cachorros',
+			background: 'banner_dog.jpg'
+		}
+	});
+};
+
+export const cats = (req: Request, res: Response) => {
+	let list = Pet.getFromType('cat')
+	res.render('pages/page', {
+		menu: createMenuObject()
+		},
+		banner: {
+			title: 'Gatos',
+			background: 'banner_cat.jpg'
+		}
+	});
+};
+
+export const fishes = (req: Request, res: Response) => {
+	let list = Pet.getFromType('fish')
+	res.render('pages/page', {
+		menu: {
+			all: false,
+			dogs: false,
+			cats: false,
+			fishes: true
+		},
+		banner: {
+			title: 'Peixes',
+			background: 'banner_fish.jpg'
+		}
+	});
+};
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+import { createMenuObject } from '../helpers/createMenuObject';
+
+export const home = (req: Request, res: Response) => {
+	res.render('pages/page', {
+		menu: createMenuObject('all'),
+		banner: {
+			title: 'Todos os animais',
+			background: 'allanimals.jpg'
+		}
+	});
+};
+
+export const dogs = (req: Request, res: Response) => {
+	res.render('pages/page', {
+		menu: createMenuObject('dogs'),
+		banner: {
+			title: 'Cachorros',
+			background: 'banner_dog.jpg'
+		}
+	});
+};
+
+export const cats = (req: Request, res: Response) => {
+	res.render('pages/page', {
+		menu: {
+			all: false,
+			dogs: false,
+			cats: true,
+			fishes: false
+		},
+		banner: {
+			title: 'Gatos',
+			background: 'banner_cat.jpg'
+		}
+	});
+};
+
+export const fishes = (req: Request, res: Response) => {
+	res.render('pages/page', {
+		menu: {
+			all: false,
+			dogs: false,
+			cats: false,
+			fishes: true
+		},
+		banner: {
+			title: 'Peixes',
+			background: 'banner_fish.jpg'
+		}
+	});
+};
